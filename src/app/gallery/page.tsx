e34bef06b61005b9b530cbb58cb61d207cc0fb78
jimmy chen <jimmy.chen@idstar.co.id>
2023-08-30 16:48:25 +0700

    setup navigation, sidebar and gallery page

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,39 @@
+import { CldImage } from "next-cloudinary";
+import UploadButton from "./upload-button";
+import cloudinary from "cloudinary";
+import { CloudinaryImage } from "./cloudinary-image";
+
+type SearchResult = {
+  public_id: string;
+};
+
+export default async function GalleryPage() {
+  const results = (await cloudinary.v2.search
+    .expression("resource_type:image")
+    .sort_by("created_at", "desc")
+    .max_results(5)
+    .execute()) as { resources: SearchResult[] };
+
+  return (
+    <section>
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-between">
+          <h1 className="text-4xl font-bold">Gallery</h1>
+          <UploadButton />
+        </div>
+
+        <div className="grid grid-cols-4 gap-4">
+          {results?.resources?.map((result) => (
+            <CloudinaryImage
+              key={result.public_id}
+              src={result.public_id}
+              alt="an image of something"
+              width="400"
+              height="300"
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
